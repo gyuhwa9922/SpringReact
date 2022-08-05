63f19bb7b0f83523b29a6eb9c56ab0930836ea73
rbghk1231@naver.com <rbghk1231@naver.com>
2022-08-05 23:47:38 +0900

    good

---
+++ todo/src/App.jsx
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Todo from './Todo';
+import AddTodo from './AddTodo';
+
+function App() {
+  return (
+    <div className="App">
+      <AddTodo/>
+      {/* <Todo/> */}
+    </div>
+  );
+}
+
+export default App;
